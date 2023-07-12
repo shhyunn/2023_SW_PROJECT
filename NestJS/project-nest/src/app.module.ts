8c613e951f31fdaf842fe9db999baf37ac0af9e2
shhyunn <limgm7@gmail.com>
2023-07-12 23:49:40 +0900

    add swagger and connect postgresSQL

--- NestJS/project-nest/src/app.module.ts
+++ NestJS/project-nest/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'PostgreSQL01',
+      password: 'limsohyun',
+      database: 'data1',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule { }
